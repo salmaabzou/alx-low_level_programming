f56c0f76e26999e799c883c0eea34890bdfe893b
salmaabzou <salma.abzou@gmail.com>
2023-04-11 08:19:32 -0700

    creating an array - add desc to return

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - create an array and initilize it with a char
+ * @size:  size of array
+ * @c: char to initialize the array
+ *
+ * Return: NULL or pointer to the array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *arr;
+
+	if (size == 0)
+		return (NULL);
+	arr = malloc(sizeof(c) * size);
+
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		arr[i] = c;
+	return (arr);
+}
